4cdc487d1ad18287cfa659d04d974b2a67b06bf2
Wero <weronika.stankiewicz94@gmail.com>
2021-03-26 22:24:44 +0100

    add state mock and first steps of vuex

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,27 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    //mock
+    tasks: [
+      {"id": 1, "title": "Zrobic zadanie", "description": null, "completed": false, "createdDate": "12 lutego 2020", "priority": "Pilne"},
+      {"id":2, "title": "Zrobic zakupy", "description": "Spozywcze",  "completed": false, "createdDate": "12 lutego 2020", "Priority": "Mało pilne"}
+    ],
   },
+  
   mutations: {
-  },
-  actions: {
+        CREATE_NEW_TASK(state, payload) {
+          state.tasks.unshift(payload)
+        }
+      },
+      actions: {
+        async createNewTask({
+          commit
+        }, payload) {
+          commit("CREATE_NEW_TASK", payload)
+        },
+      },
+      getters: {
+        getTasks: state => state.tasks
   },
   modules: {
   }
